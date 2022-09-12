85213461669958dac0f5578009e24d73dfd8e00a
Gaurav Kumar <rgaurav259@gmail.com>
2022-09-12 12:50:10 +0530

    first commit

---
+++ inventory-service/src/test/java/com/gauravtrchie/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauravtrchie.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- order-service/src/main/java/com/gauravtrchie/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/gauravtrchie/orderservice/controller/OrderController.java
@@ -2,17 +2,23 @@ package com.gauravtrchie.orderservice.controller;
 
 
 import com.gauravtrchie.orderservice.dto.OrderRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.gauravtrchie.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
 
+
+    private final OrderService orderService;
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest ){
+        orderService.placeOrder(orderRequest);
         return "Order placed Successfully";
     }
 }
